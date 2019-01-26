abb727fbd856b3b2de3bae23fd27b62128739d83
ChekieKaw <guojw08@163.com>
2019-01-26 18:02:25 +0800

    deliver

--- deliver/deliver.m
+++ deliver/deliver.m
@@ -1,3 +1,4 @@
+%读取无人机数据
 drone_state=[
     1143	1143	635	3.50000000000000	40	35	1	1	1;
 762	762	558.800000000000	8	79	40	1	1	1;
@@ -10,4 +11,58 @@ drone_state=[
 ];
 %m0=(drone_state(:,1)./1000).*(drone_state(:,2)./1000).*(drone_state(:,3)./1000).*...
 %    [2.2,8.3,5.3,7.3,7.3,7.3,7.3,7.3]';
+m_max=drone_state(:,4);
 m0=drone_state(:,4)./2;
+H=150;%飞行高度
+v_up=5;%降落速度
+T0=drone_state(:,6);
+v_t=drone_state(:,5)./60;
+t_up=(H/v_up)./60;%上升/下降时间，单位是分钟
+distance=20;%到医院的距离，千米
+
+%导入可行解
+cell={};
+n=size(cell,2);
+seq=[];%目标
+num=0;%接受方案号
+%对每一种方案做一个分配
+time_sum=1000000000;
+for i=1:n
+    %time_temp=0;
+    time_deliver_temp=10000000;
+    temp=cell2mat(cell(i));
+    m1=temp*[2,2,3]';
+    s=zeros(7,1);
+    p=size(temp,1);%包的数量，即无人机的个数
+    seq_temp=zeros(1,p);%无人机的选择
+    T1=zeros(1,p);%运送时间
+    T1(:)=100000000;
+
+    %计算每种无人机的最大飞行距离
+    %for k = 1:7
+    %    s(k)=v_t(k)*(T0(k)*m0(k)/(2*m0(k)+m1(k))-2*t_up);
+    %end
+    %暴力求解最优解
+    %对每个包裹进行计算
+    for j=1:p
+        for k=1:7 %对每种无人机，检查
+            s(k)=v_t(k)*(T0(k)*m0(k)/(2*m0(k)+m1(j))-2*t_up);
+            if s(k)>distance && m1(j)<m_max(k)
+                time_deliver_temp=distance*2/v_t(k);
+                if time_deliver_temp<T1(j);
+                    T1(j)=time_deliver_temp;
+                    seq_temp(j)=k;
+                end
+            end
+        end
+
+    end
+    time_temp=sum(T1);
+    if time_temp<time_sum
+        num=i;
+        seq=seq_temp;
+        time_sum=time_temp;
+    end
+
+
+end
